d40bc2265d0e342b6f978b6239fff746d718165b
ziad-tariq <103075559+Ziad-Tariq@users.noreply.github.com>
2026-05-21 13:00:41 +0500

    solve reverse string exercise

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(name) {
+name=name.split('').reverse().join('');
+return name;
 };
 
+reverseString('hello there')
 // Do not edit below this line
 module.exports = reverseString;
