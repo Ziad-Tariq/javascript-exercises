dce1a1473644c205e0be281f0fb3dc30fa4998b0
ziad-tariq <103075559+Ziad-Tariq@users.noreply.github.com>
2026-05-21 12:27:44 +0500

    solve repeatString exercise

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,22 @@
-const repeatString = function() {
+const repeatString = function(str , num) {
+    let nameTwo=""
+    if(num<0)
+    {
+       return "ERROR";
+    }
+    else  {
 
+    for (i=0; i<num; i++){
+    nameTwo=nameTwo.concat(str)
+
+ 
+
+    }
+    return nameTwo;
+
+    }
 };
-
+ 
+repeatString('', 10)
 // Do not edit below this line
 module.exports = repeatString;
